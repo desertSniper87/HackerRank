4db0e53d7d5c2fc7b2ec65a99ecddbb566ee8ca2
desertsniper87 <torshobuet@gmail.com>
2021-01-08 22:22:01 +0600

    fuk dis

--- hackerrank-java/src/CutTheTree.java
+++ hackerrank-java/src/CutTheTree.java
@@ -33,7 +33,6 @@ class Graph {
     }
 
     public void addEdge(Node source, Node destination) {
-
         // We make sure that every used node shows up in our .keySet()
         if (!adjacencyMap.containsKey(source))
             adjacencyMap.put(source, null);
@@ -42,7 +41,7 @@ class Graph {
             adjacencyMap.put(destination, null);
 
         addEdgeHelper(source, destination);
-//        addEdgeHelper(destination, source);
+        addEdgeHelper(destination, source);
     }
 
     public void printEdges() {
@@ -65,22 +64,49 @@ class Graph {
     }
 
 
-    public void dfsAndCut(Node start) {
+    public int dfsAndCut(Node start, int sum) {
         Stack<Node> stack = new Stack<>();
+        int sumA = 0;
+        int sumB = sum;
+        int minSum = sumB;
+
+        // System.out.println("sumA = " + sumA);
+        // System.out.println("sumB = " + sumB);
+
         boolean[] isVisited = new boolean[adjacencyMap.size() + 1];
         stack.push(start);
         while (!stack.isEmpty()) {
+            // System.out.println("stack = " + stack);
             Node current = stack.pop();
             isVisited[current.id] = true;
-            System.out.println("current.id = " + current.id);
-            try {
-                    for (Node dest : adjacencyMap.get(current)) {
-                        if (!isVisited[dest.id])
-                            stack.push(dest);
-                    }
-            } catch (NullPointerException ignored) {}
+             System.out.println("current.id = " + current.id);
+            sumB -= current.value;
+            sumA += current.value;
 
+            minSum = Math.min(Math.abs(sumB-sumA), minSum);
+
+            if (minSum == 525) {
+                System.out.println("sumA = " + sumA);
+                System.out.println("sumB = " + sumB);
+                System.out.println("current = " + current.id);
+            }
+
+            // System.out.println("sumA = " + sumA);
+            // System.out.println("sumB = " + sumB);
+
+            if (adjacencyMap.get(current) != null) {
+                for (Node dest : adjacencyMap.get(current)) {
+                    // System.out.println("LINE 97");
+                    // System.out.println("isVisited = " + Arrays.toString(isVisited));
+                    // System.out.println("dest = " + dest);
+                    if (!isVisited[dest.id]) {
+                        // System.out.println("Push");
+                        stack.push(dest);
+                    }
+                }
+            }
         }
+        return minSum;
     }
 
 }
@@ -92,18 +118,20 @@ class CutTheTreeResult {
         Graph g = new Graph();
 
         Node[] nodes = new Node[numberOfNodes + 1];
+        int sum = 0;
 
         for (int i = 1; i <= numberOfNodes; i++) {
             nodes[i] = new Node(i, data.get(i-1));
+            sum += data.get(i-1);
         }
 
         for (List<Integer> edge: edges){
             g.addEdge(nodes[edge.get(0)], nodes[edge.get(1)]);
         }
 
-        g.printEdges();
-        g.dfsAndCut(nodes[1]);
-        return 0;
+//        g.printEdges();
+
+        return g.dfsAndCut(nodes[1], sum);
     }
 
 }
@@ -113,24 +141,24 @@ public class CutTheTree {
 //        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int input = 1;
+        int input = 2;
         BufferedReader bufferedReader = new BufferedReader(new FileReader("io/CutTheTree/input."+input+".txt"));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("io/CutTheTree/output."+input+".txt"));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> data = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+                .map(Integer::parseInt)
+                .collect(toList());
 
         List<List<Integer>> edges = new ArrayList<>();
 
         IntStream.range(0, n - 1).forEach(i -> {
             try {
                 edges.add(
-                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt)
-                        .collect(toList())
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
                 );
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
