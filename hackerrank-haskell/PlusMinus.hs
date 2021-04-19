263909f3394ecf1c02783475c2a9d9a309a18a65
desertsniper87 <torshobuet@gmail.com>
2021-04-20 02:11:25 +0600

    initial commit

---
+++ hackerrank-haskell/PlusMinus.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
+
+module Main where
+
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.List
+import Data.List.Split
+import Data.Set
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+
+-- Complete the plusMinus function below.
+plusMinus :: [Int] -> Int Int Int
+plusMinus arr = 
+    where sumArr = sum arr
+
+readMultipleLinesAsStringArray :: Int -> IO [String]
+readMultipleLinesAsStringArray 0 = return []
+readMultipleLinesAsStringArray n = do
+    line <- getLine
+    rest <- readMultipleLinesAsStringArray(n - 1)
+    return (line : rest)
+
+main :: IO()
+main = do
+    n <- readLn :: IO Int
+
+    arrTemp <- getLine
+
+    let arr = Data.List.map (read :: String -> Int) . words $ arrTemp
+
+    plusMinus arr
